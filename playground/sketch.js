44e013000e414f15b5dadb6123a32b5bab93954e
Marco Mondini <mmondini@mondspace.com>
2022-01-18 23:20:00 +0100

    rising star

--- playground/sketch.js
+++ playground/sketch.js
@@ -1,98 +1,102 @@
-let config = {}
+const VoidInTheBlue = (p5) => {
+  let config = {}
 
-const rotateFromTheMiddle = (angle) => {
-  translate(config.centerX, config.centerY)
-  rotate(angle)
-  translate(-config.centerX, -config.centerY)
-}
-
-const fromTheMiddle = (callback) => {
-  push()
-  translate(config.centerX, config.centerY)
-  callback()
-  pop()
-}
-
-const randomAngle = () => {
-  return random(0, 360).toFixed()
-}
-
-const randomSunSize = () => {
-  const safeSpan = config.span
-  const breakPoint = config.width / 4
-  const limitPoint = config.width / 2
-  const start = random(safeSpan, breakPoint - safeSpan).toFixed()
-  const end = random(breakPoint, limitPoint - safeSpan).toFixed()
-  return [start, end]
-}
-
-const sunlights = (start, end) => {
-  const drawShape = () => {
-    const d = random(0, 1).toFixed()
-    circle(start, end, d)
+  const rotateFromTheMiddle = (angle) => {
+    p5.translate(config.centerX, config.centerY)
+    p5.rotate(angle)
+    p5.translate(-config.centerX, -config.centerY)
   }
 
-  fromTheMiddle(() => {
-    rotate(0)
-    fill(config.palette.one)
-    stroke(config.palette.one)
-    drawShape()
-  })
-
-  fromTheMiddle(() => {
-    rotate(PI/2)
-    fill(config.palette.two)
-    stroke(config.palette.two)
-    drawShape()
-  })
-
-  fromTheMiddle(() => {
-    rotate(PI)
-    fill(config.palette.three)
-    stroke(config.palette.three)
-    drawShape()
-  })
-
-  fromTheMiddle(() => {
-    rotate(PI*3/2)
-    fill(config.palette.four)
-    stroke(config.palette.four)
-    drawShape()
-  })
-}
-
-
-function setup() {
-  config = {
-    width: windowWidth,
-    height: windowHeight,
-    centerX: windowWidth / 2,
-    centerY: windowHeight / 2,
-    span: 0,
-    palette: {
-      background: "#123456",
-      one: "#00ffff",
-      two: "#ff0000",
-      three: "#888888",
-      four: "#ffff00"
-    },
-    items: [],
-    count: 100
+  const fromTheMiddle = (callback) => {
+    p5.push()
+    p5.translate(config.centerX, config.centerY)
+    callback()
+    p5.pop()
   }
 
-  createCanvas(config.width, config.height);
+  const randomAngle = () => {
+    return p5.random(0, 360).toFixed()
+  }
 
-  for(let i=0; i < config.count; i++) {
-    config.items.push({
-      size: randomSunSize(),
-      angle: randomAngle()
+  const randomSunSize = () => {
+    const safeSpan = config.span
+    const breakPoint = config.width / 4
+    const limitPoint = config.width / 2
+    const start = p5.random(safeSpan, breakPoint - safeSpan).toFixed()
+    const end = p5.random(breakPoint, limitPoint - safeSpan).toFixed()
+    return [start, end]
+  }
+
+  const sunlights = (start, end) => {
+    const drawShape = () => {
+      const d = p5.random(0, 1).toFixed()
+      p5.circle(start, end, d)
+    }
+
+    fromTheMiddle(() => {
+      p5.rotate(0)
+      p5.fill(config.palette.one)
+      p5.stroke(config.palette.one)
+      drawShape()
+    })
+
+    fromTheMiddle(() => {
+      p5.rotate(p5.PI/2)
+      p5.fill(config.palette.two)
+      p5.stroke(config.palette.two)
+      drawShape()
+    })
+
+    fromTheMiddle(() => {
+      p5.rotate(p5.PI)
+      p5.fill(config.palette.three)
+      p5.stroke(config.palette.three)
+      drawShape()
+    })
+
+    fromTheMiddle(() => {
+      p5.rotate(p5.PI*3/2)
+      p5.fill(config.palette.four)
+      p5.stroke(config.palette.four)
+      drawShape()
     })
   }
 
-  background(config.palette.background);
-  
-  config.items.forEach((sun) => {
-    rotateFromTheMiddle(sun.angle)
-    sunlights(sun.size[0], sun.size[1])
-  })
+
+  p5.setup = () => {
+    config = {
+      width: p5.windowWidth,
+      height: p5.windowHeight,
+      centerX: p5.windowWidth / 2,
+      centerY: p5.windowHeight / 2,
+      span: 0,
+      palette: {
+        background: "#123456",
+        one: "#00ffff",
+        two: "#ff0000",
+        three: "#888888",
+        four: "#ffff00"
+      },
+      items: [],
+      count: 6969
+    }
+
+    p5.createCanvas(config.width, config.height);
+
+    for(let i=0; i < config.count; i++) {
+      config.items.push({
+        size: randomSunSize(),
+        angle: randomAngle()
+      })
+    }
+
+    p5.background(config.palette.background);
+    
+    config.items.forEach((sun) => {
+      rotateFromTheMiddle(sun.angle)
+      sunlights(sun.size[0], sun.size[1])
+    })
+  }
 }
+
+new p5(VoidInTheBlue, 'voidInTheBlue')
--- studio/ControlledExplosion.js
+++ studio/ControlledExplosion.js
@@ -1,91 +1,92 @@
-let config = {}
+const ControlledExplosion = (p5) => {
+  let config = {}
 
-const rotateFromTheMiddle = (angle) => {
-  translate(config.centerX, config.centerY)
-  rotate(angle)
-  translate(-config.centerX, -config.centerY)
-}
-
-const fromTheMiddle = (callback) => {
-  push()
-  translate(config.centerX, config.centerY)
-  callback()
-  pop()
-}
-
-const randomAngle = () => {
-  return random(0, 360).toFixed()
-}
-
-const randomSunSize = () => {
-  const safeSpan = config.span
-  const breakPoint = config.width / 4
-  const limitPoint = config.width / 3
-  const start = random(safeSpan, breakPoint - safeSpan).toFixed()
-  const end = random(breakPoint, limitPoint - safeSpan).toFixed()
-  return [start, end]
-}
-
-const sunlights = (start, end) => {
-  const x1 = start, y1 = start, x2 = end, y2 = end
-
-  fromTheMiddle(() => {
-    rotate(0)
-    stroke(config.palette.one)
-    line(x1, y1, x2, y2) 
-  })
-
-  fromTheMiddle(() => {
-    rotate(PI/2)
-    stroke(config.palette.two)
-    line(x1, y1, x2, y2)
-  })
-
-  fromTheMiddle(() => {
-    rotate(PI)
-    stroke(config.palette.three)
-    line(x1, y1, x2, y2)
-  })
-
-  fromTheMiddle(() => {
-    rotate(PI*3/2)
-    stroke(config.palette.four)
-    line(x1, y1, x2, y2)
-  })
-}
-
-
-function setup() {
-  config = {
-    width: windowWidth,
-    height: windowHeight,
-    centerX: windowWidth / 2,
-    centerY: windowHeight / 2,
-    span: 10,
-    palette: {
-      background: "#123456",
-      one: "#00ffff",
-      two: "#ff0000",
-      three: "#888888",
-      four: "#ffff00"
-    },
-    items: [],
-    count: 100
+  const rotateFromTheMiddle = (angle) => {
+    p5.translate(config.centerX, config.centerY)
+    p5.rotate(angle)
+    p5.translate(-config.centerX, -config.centerY)
   }
 
-  createCanvas(config.width, config.height);
+  const fromTheMiddle = (callback) => {
+    p5.push()
+    p5.translate(config.centerX, config.centerY)
+    callback()
+    p5.pop()
+  }
 
-  for(let i=0; i < config.count; i++) {
-    config.items.push({
-      size: randomSunSize(),
-      angle: randomAngle()
+  const randomAngle = () => {
+    return p5.random(0, 360).toFixed()
+  }
+
+  const randomSunSize = () => {
+    const safeSpan = config.span
+    const breakPoint = config.width / 4
+    const limitPoint = config.width / 3
+    const start = p5.random(safeSpan, breakPoint - safeSpan).toFixed()
+    const end = p5.random(breakPoint, limitPoint - safeSpan).toFixed()
+    return [start, end]
+  }
+
+  const sunlights = (start, end) => {
+    const x1 = start, y1 = start, x2 = end, y2 = end
+
+    fromTheMiddle(() => {
+      p5.rotate(0)
+      p5.stroke(config.palette.one)
+      p5.line(x1, y1, x2, y2) 
+    })
+
+    fromTheMiddle(() => {
+      p5.rotate(p5.PI/2)
+      p5.stroke(config.palette.two)
+      p5.line(x1, y1, x2, y2)
+    })
+
+    fromTheMiddle(() => {
+      p5.rotate(p5.PI)
+      p5.stroke(config.palette.three)
+      p5.line(x1, y1, x2, y2)
+    })
+
+    fromTheMiddle(() => {
+      p5.rotate(p5.PI*3/2)
+      p5.stroke(config.palette.four)
+      p5.line(x1, y1, x2, y2)
     })
   }
 
-  background(config.palette.background);
-  
-  config.items.forEach((sun) => {
-    rotateFromTheMiddle(sun.angle)
-    sunlights(sun.size[0], sun.size[1])
-  })
+
+  p5.setup = () => {
+    config = {
+      width: p5.windowWidth,
+      height: p5.windowHeight,
+      centerX: p5.windowWidth / 2,
+      centerY: p5.windowHeight / 2,
+      span: 30,
+      palette: {
+        one: "#00ffff",
+        two: "#ff0000",
+        three: "#888888",
+        four: "#ffff00"
+      },
+      items: [],
+      count: 69
+    }
+
+    p5.createCanvas(config.width, config.height);
+
+    for(let i=0; i < config.count; i++) {
+      config.items.push({
+        size: randomSunSize(),
+        angle: randomAngle()
+      })
+    }
+    
+    config.items.forEach((sun) => {
+      rotateFromTheMiddle(sun.angle)
+      sunlights(sun.size[0], sun.size[1])
+    })
+  }
 }
+
+new p5(ControlledExplosion, 'controlledExplosion')
