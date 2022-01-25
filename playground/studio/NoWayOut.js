1655e01000b4c1fa9c87f3925533d7d52e5a2b56
Marco Mondini <mmondini@mondspace.com>
2022-01-25 23:01:23 +0100

    No way out

--- playground/studio/NoWayOut.js
+++ playground/studio/NoWayOut.js
@@ -1,32 +1,28 @@
 import Painter from "../canvas/painter.mjs"
 import Sun from "../canvas/sun.mjs"
 
-export const NakedContent = (p5) => {
+export const NoWayOut = (p5) => {
   let painter
   let phoenix
   let phoenixConfig = {}
   let dust
   let dustConfig = {}
 
-  const drawLettersOf = (letters) => (color, start, end) => {
-    const letter = letters[start % letters.length]
-    p5.fill(color)
-    p5.stroke(color)
-    p5.rect(start, end, start, end);
-  }
-
   p5.setup = () => {
     phoenixConfig = {
       sun: {
-        span: p5.windowWidth/3,
-        limit: 1/5
+        span: p5.windowWidth/2,
+        limit: 1/7
       },
       palette: ["#EDAE49","#FE4A49","#EDAE49","#FE4A49"],
       count: 256
     }
 
     painter = new Painter(p5, phoenixConfig)
-    phoenix = new Sun(painter, phoenixConfig, drawLettersOf('phoenix'.split('')))
+    phoenix = new Sun(painter, phoenixConfig, (color, start, end) => {
+      p5.fill(color)
+      p5.rect(start, end, start/3, end/3);
+    })
 
     dustConfig = {
       sun: {
@@ -35,10 +31,16 @@ export const NakedContent = (p5) => {
       },
       background: "#0E181B",
       palette: ["#7785AC","#b1f8f2","#7785AC","#b1f8f2"],
-      count: 512
+      count: 256
     }
 
-    dust = new Sun(painter, dustConfig, drawLettersOf('void'.split('')))
+    dust = new Sun(painter, dustConfig, (color, start, end) => {
+      p5.push()
+      p5.fill(color)
+      p5.scale(0.5)
+      p5.triangle(start, end, start/2, end/2, start/3, end);
+      p5.pop()
+    })
   }
 
   p5.draw = () => {
@@ -50,7 +52,8 @@ export const NakedContent = (p5) => {
     p5.push()
     phoenix.render()
     p5.pop()
-  
+    
+    painter.rotateClockwise()
     dust.render()
   }
 }
--- playground/studio/index.js
+++ playground/studio/index.js
@@ -2,8 +2,10 @@ import { ControlledExplosion } from "./ControlledExplosion.js"
 import { VoidInTheBlue } from "./VoidInTheBlue.js"
 import { WildPhoenix } from "./WildPhoenix.js"
 import { NakedContent } from "./NakedContent.js"
+import { NoWayOut } from "./NoWayOut.js"
 
 // new p5(VoidInTheBlue, 'layer-one')
 // new p5(ControlledExplosion, 'layer-two')
 // new p5(WildPhoenix, 'layer-one')
-new p5(NakedContent, 'layer-one')
+// new p5(NakedContent, 'layer-one')
+new p5(NoWayOut, 'layer-one')
