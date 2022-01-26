499623ddb98038e5a004610ac2314edfc0c19c65
Marco Mondini <mmondini@mondspace.com>
2022-01-26 21:57:07 +0100

    warm horizon

---
+++ playground/studio/WarmHorizon.js
@@ -0,0 +1,36 @@
+import Painter from "../canvas/painter.mjs"
+import Sun from "../canvas/sun.mjs"
+
+export const WarmHorizon = (p5) => {
+  let painter
+  let phoenix
+  let phoenixConfig = {}
+
+  p5.setup = () => {
+    phoenixConfig = {
+      sun: {
+        span: p5.windowWidth / 3,
+        limit: 1/2
+      },
+      background: "#0E181B",
+      palette: ["#EDAE49","#FE4A49","#EDAE49","#FE4A49"],
+      count: 124
+    }
+
+    painter = new Painter(p5, phoenixConfig)
+    phoenix = new Sun(painter, phoenixConfig, (color, start, end) => {
+      p5.fill(color)
+      p5.stroke(color)
+      p5.ellipse(start, end, end / 5, end / 7);
+    })
+  }
+
+  p5.draw = () => {
+    p5.background(phoenixConfig.background)
+    
+    phoenix.fillOneMoreItem()
+    
+    painter.rotateCounterClockwise()
+    phoenix.render()
+  }
+}
--- playground/studio/index.js
+++ playground/studio/index.js
@@ -3,9 +3,11 @@ import { VoidInTheBlue } from "./VoidInTheBlue.js"
 import { WildPhoenix } from "./WildPhoenix.js"
 import { NakedContent } from "./NakedContent.js"
 import { NoWayOut } from "./NoWayOut.js"
+import { WarmHorizon } from "./WarmHorizon.js"
 
 // new p5(VoidInTheBlue, 'layer-one')
 // new p5(ControlledExplosion, 'layer-two')
 // new p5(WildPhoenix, 'layer-one')
 // new p5(NakedContent, 'layer-one')
-new p5(NoWayOut, 'layer-one')
+// new p5(NoWayOut, 'layer-one')
+new p5(WarmHorizon, 'layer-one')
